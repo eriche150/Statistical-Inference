7bc4e816ffba2193db668413050d8a9d97d1f027
ehe <erhe@health.ucsd.edu>
2021-04-24 21:48:50 -0700

    courses Probability + Probability 2 are uploaded here. Information about the concepts are outlined in Notability.

--- Statistical Inference.R
+++ Statistical Inference.R
@@ -1,1 +1,15 @@
 #test 
+library(swirl)
+install_from_swirl("Statistical Inference")
+swirl()
+
+#1) Introduction 
+
+#2) Probability 1 
+deck #matrix of all the possible suits and card values. 
+
+#3) Probability 2
+mypdf #this is a function that describes the equation of the line represented by the diagonal of rectangle. 
+#as the PDF, it is the derivative of F(x), the CDF. 
+integrate(mypdf, lower=0, upper=1.6) #calculate area of this equation between x=0 and x=1.6 
+
