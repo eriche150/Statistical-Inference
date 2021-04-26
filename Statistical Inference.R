132e7844f88e46523dd5b5d1af9b330e0c6ce4b5
ehe <erhe@health.ucsd.edu>
2021-04-26 15:10:33 -0700

    updated w/ function for expected values for dice counts

--- Statistical Inference.R
+++ Statistical Inference.R
@@ -13,3 +13,11 @@ mypdf #this is a function that describes the equation of the line represented by
 #as the PDF, it is the derivative of F(x), the CDF. 
 integrate(mypdf, lower=0, upper=1.6) #calculate area of this equation between x=0 and x=1.6 
 
+#5) Expected Value 
+expect_dice #function w/ 6 number-long array. 
+#function(pmf){ mu <- 0; for (i in 1:6) mu <- mu + i*pmf[i]; mu}
+dice_high
+expect_dice(dice_high) #biased dice that has higher roll counts
+expect_dice(dice_low) #biased dice that has lower roll counts 
+
+0.5*(edh+edl)
